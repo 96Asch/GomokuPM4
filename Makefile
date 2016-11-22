cc4788aabaabbc1965e450b46279f639c37022ee
ashuang <A.S.Huang@student.tudelft.nl>
2016-11-22 12:35:33 +0100

    Add pointer based stack

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 # a simple makefile
-all: main.o goboard.o
-	g++ -Wall -o GomokuGame goboard.o main.o
+all: main.o goboard.o stack.o
+	g++ -Wall -o GomokuGame goboard.o main.o stack.o
 gobord.o: goboard.cc goboard.h
 	g++ -Wall -c goboard.cc
-main.o: main.cc goboard.h
+main.o: main.cc goboard.h stack.h
 	g++ -Wall -c main.cc
+stack.o: stack.cc stack.h
+	g++ -Wall -c stack.cc
--- main.cc
+++ main.cc
@@ -2,11 +2,23 @@
 #include <iostream>
 
 #include "goboard.h"
+#include "stack.h"
+
 using namespace std;
 
 int main ( ) {
   Goboard Gobord;
-  Gobord.print();
-  cout << "hello" << endl;
+
+  Stack stack;
+  stack.push(1,2);
+  stack.push(3,4);
+
+  int from, to;
+  stack.pop(from, to);
+  cout << from << ", " << to << endl;
+  cout << stack.isEmpty() << endl;
+  stack.pop(from, to);
+  cout << from << ", " << to << endl;
+  cout << stack.isEmpty() << endl;
   return 0;
 }//main

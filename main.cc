4483525e2f4382020372cf09ab1dc58faa0476bb
deflesdop <micha1998@gmail.com>
2016-11-23 14:42:06 +0100

    leesgetal leesinput

--- main.cc
+++ main.cc
@@ -4,6 +4,41 @@
 #include "goboard.h"
 using namespace std;
 
+// Returns a user given option.
+char getOption(char invoer) {
+	while (invoer == '\n') {
+		cin.get(invoer);
+	}
+	return invoer;
+}
+
+// Converts a stream chars into a single int.
+void convertCharstoInt(char letter, int & numberToCheck) {
+	int temp = letter - '0';
+	numberToCheck *= 10;
+	numberToCheck += temp;
+}
+
+// Returns the digits of a stream of chars up to maxNumber.
+int readDigit(int maxNumber) {
+	int res = 0;
+	char ch = '\n';
+	while (!isdigit(ch)) {
+	cin.get(ch);
+	}
+	convertCharstoInt(ch, res);
+	while (ch != '\n') {
+		cin.get(ch);
+		if (isdigit(ch) && res < maxNumber) {
+			convertCharstoInt(ch, res);
+		}
+	}
+	if (res > maxNumber) {
+		res /= 10;
+	}
+	cout << "You entered: " << res << endl;
+	return res;
+}
 int main ( ) {
   Goboard Gobord;
   Gobord.print();
